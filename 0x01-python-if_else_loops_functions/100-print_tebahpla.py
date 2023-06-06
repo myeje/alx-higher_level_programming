ddb1b0b816e69b71a5abe29fcedc60860f4002d1
Joshua Emore <ehimareemore@gmail.com>
2023-06-06 22:12:56 +0000

    Prints the ASCII alphabet in reverse, alternating lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for j in range(122, 96, -1):
     if j % 2 == 0:
-        print(chr(j), end='')
+        print("{}".format(chr(j)), end='')
     else:
-        print(chr(j - 32), end='')
+        print("{}".format(chr(j - 32)), end='')
