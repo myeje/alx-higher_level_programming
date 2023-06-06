8f1acce7e96ce207b8065e60d4915f15b84e9533
Joshua Emore <ehimareemore@gmail.com>
2023-06-06 19:05:22 +0000

    Prints every combination of numbers in a certain range

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,5 @@ for i in range(0, 9):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
             print("{}{}".format(i, j))
+            break
         print("{}{}, ".format(i, j), end='')
