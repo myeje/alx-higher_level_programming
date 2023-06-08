a67595d6c79cb4079a56bd5375c358fee831fdf6
Joshua Emore <ehimareemore@gmail.com>
2023-06-08 21:56:47 +0000

    Program that imports all functions from a file and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,22 @@
-#!/usr/bin/python3
+#i/usr/bin/python3
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     amount = len(sys.argv) - 1
 
+    a = sys.argv[1]
+    b = sys.argv[3]
     if amount != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     elif sys.argv[2] == '+':
-        print("{} + {} = {}"
-            .format(sys.argv[1], sys.argv[3], add(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} + {} = {}".format(a, b, add(int(a), int(b))))
     elif sys.argv[2] == '-':
-        print("{} + {} = {}"
-            .format(sys.argv[1], sys.argv[3], sub(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} + {} = {}".format(a, b, sub(int(a), int(b))))
     elif sys.argv[2] == '*':
-        print("{} + {} = {}"
-            .format(sys.argv[1], sys.argv[3], mul(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} + {} = {}".format(a, b, mul(int(a), int(b))))
     elif sys.argv[2] == '/':
-        print("{} + {} = {}"
-            .format(sys.argv[1], sys.argv[3], div(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} + {} = {}".format(a, b, div(int(a), int(b))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
