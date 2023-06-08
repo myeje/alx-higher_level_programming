c520d24feb43d1b9c8f8561827dd2e51a111beea
Joshua Emore <ehimareemore@gmail.com>
2023-06-08 22:13:05 +0000

    Program that imports all functions from a file and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,22 @@
-#i/usr/bin/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     amount = len(sys.argv) - 1
 
-    a = sys.argv[1]
-    b = sys.argv[3]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     if amount != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     elif sys.argv[2] == '+':
-        print("{} + {} = {}".format(a, b, add(int(a), int(b))))
+        print("{} + {} = {}".format(a, b, add(a, b)))
     elif sys.argv[2] == '-':
-        print("{} + {} = {}".format(a, b, sub(int(a), int(b))))
+        print("{} + {} = {}".format(a, b, sub(a, b)))
     elif sys.argv[2] == '*':
-        print("{} + {} = {}".format(a, b, mul(int(a), int(b))))
+        print("{} + {} = {}".format(a, b, mul(a, b)))
     elif sys.argv[2] == '/':
-        print("{} + {} = {}".format(a, b, div(int(a), int(b))))
+        print("{} + {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
