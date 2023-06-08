420a6c925622d517f2874892d577ad4a08acdccc
Joshua Emore <ehimareemore@gmail.com>
2023-06-08 21:51:48 +0000

    Program that imports all functions from a file and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,13 +8,17 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     elif sys.argv[2] == '+':
-        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], add(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} + {} = {}"
+            .format(sys.argv[1], sys.argv[3], add(int(sys.argv[1]), int(sys.argv[3]))))
     elif sys.argv[2] == '-':
-        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], sub(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} + {} = {}"
+            .format(sys.argv[1], sys.argv[3], sub(int(sys.argv[1]), int(sys.argv[3]))))
     elif sys.argv[2] == '*':
-        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], mul(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} + {} = {}"
+            .format(sys.argv[1], sys.argv[3], mul(int(sys.argv[1]), int(sys.argv[3]))))
     elif sys.argv[2] == '/':
-        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], div(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} + {} = {}"
+            .format(sys.argv[1], sys.argv[3], div(int(sys.argv[1]), int(sys.argv[3]))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
