3796a52380566cafdf927743cd8d414fcc4f13fe
Joshua Emore <ehimareemore@gmail.com>
2023-06-12 13:05:43 +0000

    Finction retrieves an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,8 @@
 def element_at(my_list, idx):
     if idx < 0:
         return("None")
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return("None")
     else:
         return(my_list[idx])
+
