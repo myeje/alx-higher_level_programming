185fb317827c2c7c229063dae31df6e6f5dd5846
Joshua Emore <ehimareemore@gmail.com>
2023-06-13 00:22:27 +0000

    CPython #0: Python list(??????)

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -10,16 +10,11 @@
 void print_python_list_info(PyObject *p)
 {
 	Py_ssize_t size = PyList_Size(p), i;
-	PyObject *unit, *sub;
+	/*PyObject *unit, *sub;*/
 	PyListObject *temp = (PyListObject *)p;
 
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", temp->allocated);
 	for (i = 0; i < size; i++)
-	{
-		unit = PyList_GetItem(p, i);
-		sub = PyObject_Repr(unit);
-		printf("Element %ld : %s\n", i, PyUnicode_AsUTF8(sub));
-		Py_DECREF(sub);
-	}
+		printf("Element %ld : %s\n", i, Py_TYPE(temp->ob_item[i])->tp_name);
 }
