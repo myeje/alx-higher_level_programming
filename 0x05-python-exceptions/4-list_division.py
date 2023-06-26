2c90d65c1f63a66bd71ce9ea076894fadc285400
Joshua Emore <ehimareemore@gmail.com>
2023-06-26 13:51:22 -0700

    Function that divides element by element 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     divide = 0
     for i in range(list_length):
         try:
-            if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
+            if isinstance(my_list_1[i] and my_list_2[i], (int, float)):
                 divide = my_list_1[i] / my_list_2[i]
             else:
                 raise TypeError
