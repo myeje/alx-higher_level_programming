4e58f87c84c781e070150c7358f4e816b97637ea
Joshua Emore <ehimareemore@gmail.com>
2023-06-26 12:02:02 -0700

    Prints x elements of a string

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    amount = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
+    try:
+        amount = 0
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
             amount += 1
-        except:
-            continue
-    print()
-    return (amount)
+        print()
+        return (amount)
+    except:
+        print()
+        return (amount)
