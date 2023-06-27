81bcca8c5d5ff5d45da3c8eb023e04d1c1df8fe9
Joshua Emore <ehimareemore@gmail.com>
2023-06-27 13:04:21 -0700

    Creating a class with an instance of size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Define a square
+Has a private instance attribute: size
+"""
+
+class Square:
+    """
+    Class with an instance attribut size
+    """
+    def __init__(self, size):
+        """
+        creating an instance of square that represents its size
+        """
+        self.__size = size
