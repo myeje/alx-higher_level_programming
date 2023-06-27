7bc20ccaeda8480a592f54ebcac797ff8df10e73
Joshua Emore <ehimareemore@gmail.com>
2023-06-27 14:12:57 -0700

    Defines a square with exceptions to the attribute

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,19 +6,20 @@ Has a private instance attribute: size
 
 
 class Square:
-        """
-        Class Square
-        """
-        def __init__(self, size=0):
-            """
-            creating an instance of square that represents its size
-            Raises TypeError if instance is not an integer
-            or ValueError if instance is less than 0
-            """
+    """
+    Class Square
+    """
 
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
+    def __init__(self, size=0):
+        """
+        creating an instance of square that represents its size
+        Raises TypeError if instance is not an integer
+        or ValueError if instance is less than 0
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
