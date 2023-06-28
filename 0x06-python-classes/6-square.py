049128539c9a846befd641697a3c77e923a33b90
Joshua Emore <ehimareemore@gmail.com>
2023-06-27 21:54:45 -0700

    Defining a square with position attribute

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,7 +50,7 @@ class Square:
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if not isinstance(value[0], int) or value[0] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")  
+            raise TypeError("position must be a tuple of 2 positive integers")
         if not isinstance(value[1], int) or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
