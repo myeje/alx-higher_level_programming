5a5b19f585c809cee916b2ad74aca2be11d73bc8
Joshua Emore <ehimareemore@gmail.com>
2023-07-02 14:51:32 -0700

    Functions that divides all elements of a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,14 +10,14 @@ def matrix_divided(matrix, div):
     Return: new matrix
 
     Raise:
-    TypeError: if number not int 
-    or matrix row not the same size
-    ZeroDivisionError: if div is 0
+        TypeError: if number not int
+            or matrix row not the same size
+        ZeroDivisionError: if div is 0
     """
-    if not all(isinstance(row, list) and all(isinstance(value, (int, float)) 
-            for value in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists)" 
-                " of integers/float")
+    if not all(isinstance(row, list) and all(isinstance(value, (int, float))
+               for value in row) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) "
+                        "of integers/float")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
