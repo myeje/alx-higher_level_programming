2dec656a085c9f03ef32f410b4e3dd347449e085
Joshua Emore <ehimareemore@gmail.com>
2023-07-10 17:08:16 -0700

    Public instance that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class and function"""
+
+
+class BaseGeometry():
+    """
+    empty class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
