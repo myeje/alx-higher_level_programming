eab518d250ebf263922470ab57ef7414830dc8d5
Joshua Emore <ehimareemore@gmail.com>
2023-07-11 14:59:16 -0700

    Function that returns the json repr of an object

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import json
 """Python module"""
+import json
 
 
 def to_json_string(my_obj):
     """Function that returns the JSON repr of an object"""
-    return (json.dumps(my_obj))
+    return json.dumps(my_obj)
