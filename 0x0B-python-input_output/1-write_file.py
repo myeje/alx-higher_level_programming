0b52f5b5e4c9edb584e2e83f6b0820f12e5d76d7
Joshua Emore <ehimareemore@gmail.com>
2023-07-11 14:30:40 -0700

    Function that write a string to a text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+"""Python function"""
+
+
+def write_file(filename="", text=""):
+    """Function that writes a string to a text file"""
+    with open(filename, mode="w", encoding="utf-8") as myFile:
+        return (myFile.write(text))
