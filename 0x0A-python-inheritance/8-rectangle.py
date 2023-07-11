515c08a6297b5b0343cafc591876fac3554076b6
Joshua Emore <ehimareemore@gmail.com>
2023-07-10 18:01:24 -0700

    Rectangle class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/in/python3
+"""Rectangle class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class rectangle with inherited clas BaseGeometry"""
+    def __init__(self, width, height):
+        """
+        Function initializes a rectangle class
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
