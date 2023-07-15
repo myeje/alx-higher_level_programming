cae95ba00e805e4798ee24eb8dba9e411572bc76
Joshua Emore <ehimareemore@gmail.com>
2023-07-15 12:40:00 -0700

    Class rectangle(__str__ update)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -69,3 +69,8 @@ class Rectangle(Base):
         """Function that print the rectanglee instance with char '#'"""
         for column in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        """function that returns a string"""
+        output = "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__, self.id, self.x, self.y, self.width, self.height)
+        return (output)
