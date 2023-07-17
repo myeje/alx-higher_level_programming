e5cd644d5242a2438e6c0e67e86166a19abe012d
Joshua Emore <ehimareemore@gmail.com>
2023-07-16 18:08:32 -0700

    Class Base(Static method thatreturns the JSON str repr of list_dictionaries)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Class Base"""
+import json
 
 
 class Base:
@@ -13,3 +14,9 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == []:
+            return ("[]")
+        return (json.dumps(list_dictionaries))
