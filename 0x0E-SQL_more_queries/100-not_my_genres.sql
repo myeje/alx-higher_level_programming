4e59234988e694276a662da73491f44a5df37ee5
Joshua Emore <ehimareemore@gmail.com>
2023-08-16 19:53:32 -0700

    a script that uses a database to list all genres not linked to a show

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- A script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+SELECT tv_genres.name
+FROM tv_genres
+LEFT JOIN (
+    SELECT genre_id
+    FROM tv_show_genres
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
+) AS dexter_genres ON tv_genres.id = dexter_genres.genre_id
+WHERE dexter_genres.genre_id IS NULL
+ORDER BY tv_genres.name ASC;
