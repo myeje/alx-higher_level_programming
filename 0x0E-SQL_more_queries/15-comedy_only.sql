8b34c0b6768647499f402f76c6ef212ebcea4640
Joshua Emore <ehimareemore@gmail.com>
2023-08-16 19:25:16 -0700

    a script that lists all Comedy shows in the database

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,6 +2,6 @@
 SELECT tv_shows.title
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_shows_genres.show_id = tv_shows.id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
