180e6fc62b055f987a6244ffbf46fe3784ccb2f6
Joshua Emore <ehimareemore@gmail.com>
2023-08-16 17:39:12 -0700

    a script that lists all cities contained in the database

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- A script that lists all cities contained in the database hbtn_0d_usa.
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.stae_id = states.id
+ORDER BY cities.id ASC;
