dda5a199158c5d861bc7b9fcc630cd30c848bfca
Joshua Emore <ehimareemore@gmail.com>
2023-09-12 14:59:26 +0100

    Class rectangle with constructor and if statement

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 /**
  * Class Rectangle that defines a rectangle with 2 constructors
+ * and an if statement
  */
 class Rectangle {
 	constructor (w, h) {
-		this.width = w;
-		this.height = h;
+		if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+			this.width = w;
+			this.height = h;
+		}
 	}
 }
 
