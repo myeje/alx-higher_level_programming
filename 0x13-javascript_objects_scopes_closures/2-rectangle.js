c82586f2e5bb56b98ebd0d1bd701241eb566fdfe
Joshua Emore <ehimareemore@gmail.com>
2023-09-12 17:13:53 +0100

    Class rectangle with constructor and if statement

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,8 @@
  * Class Rectangle that defines a rectangle with 2 constructors
  * and an if statement
  * 
- * instance method print that prints out a char
+ * 
+ * 
  */
 class Rectangle {
 	constructor (w, h) {
@@ -12,17 +13,4 @@ class Rectangle {
 			this.height = h;
 		}
 	}
-
-	print() {
-		for (let i = 0; i < this.height; i++) {
-			let varName = '';
-			let j = 0;
-			for (; j < this.width; j++) {
-				varName += 'X';
-			}
-			console.log (varName);
-			}
-		}
-	}
-
-module.exports = Rectangle;
+}
