57778a0daa7cdf20dc1b38c31f4528debc436d16
Joshua Emore <ehimareemore@gmail.com>
2023-09-13 02:00:51 +0100

    function returns a reversed list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/**
+ * function that returns the reversed version of a list
+ */
+exports.esrever = function (list) {
+	let num = list.length - 1;
+	const list2 = [];
+
+	for (; num >= 0; num--) {
+		list2.push(list[num]);
+	}
+
+	return list;
+};
