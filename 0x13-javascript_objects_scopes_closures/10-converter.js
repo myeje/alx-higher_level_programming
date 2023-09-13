0e0515755e7adb653bbaf5e18ca2449046dae473
Joshua Emore <ehimareemore@gmail.com>
2023-09-13 02:19:39 +0100

    Function converts number to a chosen base

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.converter = function (base) {
+	if (base <= 1 || base > 36) {
+		return null;
+	}
+
+	function convert (i) {
+		return i.tostring(base);
+	}
+	return convert;
+}
