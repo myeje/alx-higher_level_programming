45ba7eb407d9429526ef8860d124198333bb6419
Joshua Emore <ehimareemore@gmail.com>
2023-09-13 15:59:27 +0100

    script imports a dict of occurrences by userId

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,17 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
+
+const { dict } = require('./101-data.js');
 
 const newDict = {};
 
-Object.getOwnPropertyNames(dict).forEach(occurences => {
-  if (newDict[dict[occurences]] === undefined) {
-    newDict[dict[occurences]] = [occurences];
-  } else {
-    newDict[dict[occurences]].push(occurences);
-  }
-});
+for (const user in dict) {
+	const occur = dict[user];
+
+	if (newDict[occur] === undefined) {
+		newDict[occur] = [user];
+	} else {
+		newDict[occur].push(user);
+	}
+}
+
 console.log(newDict);
