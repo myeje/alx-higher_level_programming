f2ddfbe829cfe5b37d993b57a4b7cc8645db09aa
Joshua Emore <ehimareemore@gmail.com>
2023-09-13 20:05:15 -0700

    Second biggest num

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,8 @@
 const arg = process.argv.slice(2).map(Number);
 
 if (arg.length <= 1) {
-	console.log(0);
+  console.log(0);
 } else {
-	arg.sort((a, b) => b - a);
-	console.log(arg[1]);
+  arg.sort((a, b) => b - a);
+  console.log(arg[1]);
 }
