55f38ea101ebde961b5a0c4a0d30ed1495546a40
Joshua Emore <ehimareemore@gmail.com>
2023-09-13 18:22:30 -0700

    code fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const arg = process.argv[2]
+const arg = process.argv[2];
 
 if (parseInt(arg)) {
-	console.log(`My number: ${parseInt(arg)}`);
+  console.log(`My number: ${parseInt(arg)}`);
 } else {
-	console.log('Not a number');
+  console.log('Not a number');
 }
