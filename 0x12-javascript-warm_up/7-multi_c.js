365f1fc98ac69544a3e9e4e263a1cf97ce710a7b
Joshua Emore <ehimareemore@gmail.com>
2023-09-13 18:45:13 -0700

    code fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 const num = process.argv[2];
 
 if (parseInt(num)) {
-	for (let i = 0; i < num; i++) {
-		console.log('C is fun');
-	}
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
 } else {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
