bd15d7fa46c74433f20374d33cb04df56831f3f2
Joshua Emore <ehimareemore@gmail.com>
2023-09-14 02:28:43 +0100

    Printing multi line using array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const line = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < lines.length; i++) {
 	console.log(lines[i]);
