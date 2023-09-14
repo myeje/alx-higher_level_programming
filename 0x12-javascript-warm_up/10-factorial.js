7cccc5fb96309f399e3fffaf66358eb1071ce0e8
Joshua Emore <ehimareemore@gmail.com>
2023-09-13 19:57:42 -0700

    Factorial function using recursion

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,13 +2,13 @@
 const arg = parseInt(process.argv[2]);
 
 function factorial (i) {
-	if (isNaN(i) || i < 0) {
-		return 1;
-	}
-	if (i === 0) {
-		return 1;
-	}
-	return i * factorial(i - 1);
+  if (isNaN(i) || i < 0) {
+    return 1;
+  }
+  if (i === 0) {
+    return 1;
+  }
+  return i * factorial(i - 1);
 }
 
 console.log(factorial(arg));
