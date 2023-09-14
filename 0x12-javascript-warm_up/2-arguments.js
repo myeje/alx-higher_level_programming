00ba0f8b2c32ed881037eb3fae97f727b9e276a7
Joshua Emore <ehimareemore@gmail.com>
2023-09-14 01:29:53 +0100

    Counting args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const args = process.argv.length;
 
-if (args === 0) {
-	console.log('No argument');
-} else if (args === 1) {
+if (args > 2) {
+	console.log('Argument found');
+} else if (args > 3) {
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
