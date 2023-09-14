444c8049170f7d89b5bda189972b74f18d5ac4cf
Joshua Emore <ehimareemore@gmail.com>
2023-09-13 19:03:06 -0700

    Add function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 const first = process.argv[2];
 const second = process.argv[3];
 
-function add(a, b) {
-	return (a + b);
+function add (a, b) {
+  return (a + b);
 }
 console.log(add(parseInt(first), parseInt(second)));
