c4cdb8b7f9924c3cba8aa0bf6b22a2e6a4ffa497
Joshua Emore <ehimareemore@gmail.com>
2023-10-03 02:59:52 +0100

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script takes in a URL, sends a request to that URL
-# ...and displas the size of the body of the response
+# Bash script takes in a URL, sends a request to it and displays the size
 curl -s "$1" | wc -c
