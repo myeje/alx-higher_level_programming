4b9693a611990ef936bbba8562963db442f635d5
Joshua Emore <ehimareemore@gmail.com>
2023-11-06 12:53:00 +0100

    Task 7

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
 	$.get("https://swapi-api.alx-tools.com/api/people/5/?format=json",
 	function(data) {
 	  var characterName = data.name;
-	  $("#character").text(characterName);
+	  $("DIV#character").text(characterName);
 	});
 });
