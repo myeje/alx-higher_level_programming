36c139e367259287f4f519772270b649505e5eb1
Joshua Emore <ehimareemore@gmail.com>
2023-11-06 12:53:37 +0100

    Task 8

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 		"https://swapi-api.alx-tools.com/api/films/?format=json",
 		function (data) {
 			data.results.forEach(function (film) {
-				$("<li>").text(film.title).appendTo("ul#list_movies");
+				$('UL#list_movies').append(...data.results.map(film => `<li>${film.title}</li>`));
 			});
 		}
 	);
